d570e7d98fdcaf21db0b474fe4ba7c58ef3b89ab
Nekoi-Mirai <blackcatjino@gmail.com>
2023-03-01 20:14:42 +1100

    Lab-118

---
+++ analyze-insulin.py
@@ -0,0 +1,30 @@
+import re
+
+with open('preproinsulin-seq.txt','r') as file:
+    content = file.read()
+    
+content = re.sub(r'\/\/|\s|\r|\n|\d+|ORIGIN', '', content)
+print(content)
+with open('preproinsulin-seq-clean.txt','w') as file:
+    file.write(content)
+
+lsinsulin = content[0:24]
+binsulin = content[24:54]
+cinsulin = content[54:89]
+ainsulin = content[89:110]
+
+with open('lsinsulin-seq-clean.txt','w') as file:
+    file.write(lsinsulin)
+    
+with open('binsulin-seq-clean.txt','w') as file:
+    file.write(binsulin)
+    
+with open('cinsulin-seq-clean.txt','w') as file:
+    file.write(cinsulin)
+    
+with open('ainsulin-seq-clean.txt','w') as file:
+    file.write(ainsulin)
+print("Isinsulin:", lsinsulin)
+print("bInsulin:",binsulin)
+print("cInsulin:",cinsulin)
+print("aInsulin:",ainsulin)
